589a6fafc20063d14201f8092f9aa79cdc843d3a
Romain Warnan <romain.warnan@gmail.com>
2018-02-16 20:14:45 +0100

    Simplification Java

--- src/test/kotlin/Natas8Test.kt
+++ src/test/kotlin/Natas8Test.kt
@@ -1,22 +1,19 @@
 
+import org.junit.Assert
 import org.junit.Test
 import java.nio.charset.Charset
 import java.util.*
 import javax.xml.bind.DatatypeConverter
 
 class Natas8Test {
+
     private val encodedSecret = "3d3d516343746d4d6d6c315669563362"
-    /*
-        $encodedSecret = "3d3d516343746d4d6d6c315669563362";
-        function encodeSecret($secret) {
-            return bin2hex(strrev(base64_encode($secret)));
-        }
-     */
+
     @Test
     fun decode() {
         val bin = DatatypeConverter.parseHexBinary(encodedSecret)
         val string = String(bin, Charset.defaultCharset()).reversed()
-        val code = String(Base64.getMimeDecoder().decode(string), Charset.forName("ISO-8859-1"))
-        println(code)
+        val code = String(Base64.getDecoder().decode(string), Charset.defaultCharset())
+        Assert.assertEquals("oubWYf2kBq", code)
     }
 }
